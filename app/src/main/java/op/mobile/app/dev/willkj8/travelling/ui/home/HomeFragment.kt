298cba3bb7b7ceaf56a4a7fc185f33adfc544b74
Kaylem W <willkj8@student.op.ac.nz>
2022-03-25 15:58:12 +1300

    feat: add tools button and country name

--- app/src/main/java/op/mobile/app/dev/willkj8/travelling/ui/home/HomeFragment.kt
+++ app/src/main/java/op/mobile/app/dev/willkj8/travelling/ui/home/HomeFragment.kt
@@ -5,9 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import op.mobile.app.dev.willkj8.travelling.R
+import op.mobile.app.dev.willkj8.travelling.databinding.FragmentHomeBinding
 import op.mobile.app.dev.willkj8.travelling.ui.login.LoginFragmentDirections
 
 class HomeFragment : Fragment() {
@@ -17,24 +20,23 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_home, container, false)
-        val btnToTranslator: Button = view.findViewById(R.id.btn_to_translator)
-        val btnToQuiz: Button = view.findViewById(R.id.btn_to_quiz)
-        val btnToSettings: Button = view.findViewById(R.id.btn_to_settings)
+        val btnToTools: Button = view.findViewById(R.id.btn_to_tools)
 
-        btnToTranslator.setOnClickListener {
+        btnToTools.setOnClickListener {
             val action = HomeFragmentDirections
-                .actionHomeFragmentToTranslatorFragment()
+                .actionHomeFragmentToToolsFragment()
             view?.findNavController()?.navigate(action)
         }
-        btnToQuiz.setOnClickListener {
-            val action = HomeFragmentDirections
-                .actionHomeFragmentToQuizFragment()
-            view?.findNavController()?.navigate(action)
-        }
-        btnToSettings.setOnClickListener {
-            val action = HomeFragmentDirections
-                .actionHomeFragmentToSettingsFragment()
-            view?.findNavController()?.navigate(action)
+
+        val binding: FragmentHomeBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_home, container, false
+        )
+
+        val viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+        binding.apply {
+            lifecycleOwner = viewLifecycleOwner
+            homeViewModel = viewModel
+            return root
         }
 
         return view
