9e98ed00ba2be344ff4eb48a8b55d9e451291149
Kaylem W <willkj8@student.op.ac.nz>
2022-04-04 10:02:39 +1200

    feat: update binding adapter

--- app/src/main/java/op/mobile/app/dev/willkj8/travelling/helpers/BindingAdapter.kt
+++ app/src/main/java/op/mobile/app/dev/willkj8/travelling/helpers/BindingAdapter.kt
@@ -1,21 +1,53 @@
-package op.mobile.app.dev.willkj8.travelling
+package op.mobile.app.dev.willkj8.travelling.helpers
 
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
+import op.mobile.app.dev.willkj8.travelling.R
 import op.mobile.app.dev.willkj8.travelling.api.ServiceStatus
+import op.mobile.app.dev.willkj8.travelling.helpers.recyclerview.CountryRVAdapter
+import op.mobile.app.dev.willkj8.travelling.model.Country
+
+@BindingAdapter("country_list_data")
+fun setCountryListData(rv: RecyclerView, data: List<Country>?) {
+    val adapter = rv.adapter as CountryRVAdapter
+    adapter.submitList(data)
+}
 
 @BindingAdapter("service_status")
-fun bindServiceStatus(tvStatus: TextView, status: ServiceStatus?) {
+fun setServiceStatus(tv: TextView, status: ServiceStatus?) {
     when (status) {
         ServiceStatus.LOADING -> {
-            tvStatus.visibility = View.VISIBLE
-            tvStatus.text = "Loading..."
+            tv.visibility = View.VISIBLE
+            tv.text = tv.context.getString(R.string.loading)
         }
         ServiceStatus.ERROR -> {
-            tvStatus.visibility = View.VISIBLE
-            tvStatus.text = "Connection Error"
+            tv.visibility = View.VISIBLE
+            tv.text = tv.context.getString(R.string.connection_error)
         }
-        ServiceStatus.COMPLETE -> tvStatus.visibility = View.GONE
+        ServiceStatus.COMPLETE -> tv.visibility = View.GONE
     }
+}
+
+@BindingAdapter("round_image")
+fun setRoundImage(iv: ImageView, imageUrl: String) {
+    Glide.with(iv.context)
+        .load(imageUrl)
+        .apply(RequestOptions().circleCrop())
+        .into(iv)
+}
+
+@BindingAdapter("square_image")
+fun setSquareImage(iv: ImageView, imageUrl: String) {
+    Glide.with(iv.context)
+        .load(imageUrl)
+        .apply(
+            RequestOptions.bitmapTransform(RoundedCorners(8))
+        )
+        .into(iv)
 }
