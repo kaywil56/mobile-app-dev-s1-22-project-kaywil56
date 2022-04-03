ef6f1b7f7f88764f89bc9c5be8da5bdac2360a1d
Kaylem W <willkj8@student.op.ac.nz>
2022-04-04 10:03:04 +1200

    feat: remove img field

--- app/src/main/java/op/mobile/app/dev/willkj8/travelling/model/Country.kt
+++ app/src/main/java/op/mobile/app/dev/willkj8/travelling/model/Country.kt
@@ -1,3 +1,21 @@
-package op.mobile.app.dev.willkj8.travelling
+package op.mobile.app.dev.willkj8.travelling.model
 
-data class Country(val id: String, val name: String)
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import kotlinx.parcelize.RawValue
+
+/**
+ * Unlike strings, integers and booleans, user defined objects can not be passed
+ * between Fragments.
+ *
+ * Resource: https://developer.android.com/kotlin/parcelize
+ */
+@Parcelize
+data class Country(
+    val id: Int,
+    val name: String,
+    val flagImg: String,
+    val langCode: String,
+    val phrases: List<String>,
+    val quiz: @RawValue List<Quiz> // Serialize List of Quiz
+) : Parcelable
