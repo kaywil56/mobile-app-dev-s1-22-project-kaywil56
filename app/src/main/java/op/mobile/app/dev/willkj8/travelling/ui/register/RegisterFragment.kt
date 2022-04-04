4d165c785960788ac2d3947bca51c9bf2ae845b3
Kaylem W <willkj8@student.op.ac.nz>
2022-04-04 22:37:25 +1200

    feat: import firebase

--- app/src/main/java/op/mobile/app/dev/willkj8/travelling/ui/register/RegisterFragment.kt
+++ app/src/main/java/op/mobile/app/dev/willkj8/travelling/ui/register/RegisterFragment.kt
@@ -5,25 +5,72 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import com.google.firebase.auth.FirebaseAuth
 import op.mobile.app.dev.willkj8.travelling.R
 
-class RegisterFragment : Fragment() {
+class RegisterFragment: Fragment() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
+    ): View? {
         val view = inflater.inflate(R.layout.fragment_register, container, false)
+
+        // Get a new instance of Firebase authentication
+        auth = FirebaseAuth.getInstance()
+
         val btnRegister: Button = view.findViewById(R.id.btn_register)
+        val etEmailAddress: EditText = view.findViewById(R.id.et_email_address)
+        val etPassword: EditText = view.findViewById(R.id.et_password)
+        val etConfirmPassword: EditText = view.findViewById(R.id.et_confirm_password)
 
         btnRegister.setOnClickListener {
-            val action = RegisterFragmentDirections
-                .actionRegisterFragmentToLoginFragment()
-            view?.findNavController()?.navigate(action)
+            val email = etEmailAddress.text.toString()
+            val password = etPassword.text.toString()
+            val confirmPassword = etConfirmPassword.text.toString()
+
+            // Set of validation rules
+            // You will write some code here for your formative assessment
         }
 
         return view
+    }
+
+    /**
+     * This function registers a new user using Firebase authentication
+     *
+     * @param email the new user's email address
+     * @param password the new user's password
+     */
+    private fun register(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    // Indicate to the application user that a new user has been created
+                    Toast.makeText(
+                        activity,
+                        "User created.",
+                        Toast.LENGTH_LONG
+                    ).show()
+
+                    // Navigate to the login screen. Check mobile_navigation.xml for this action
+                    view?.findNavController()?.navigate(
+                        RegisterFragmentDirections.actionRegisterFragmentToLoginFragment()
+                    )
+                } else {
+                    // Indicate to the application user if the email address already exists
+                    Toast.makeText(
+                        activity,
+                        "Email address already exists.",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
     }
 }
