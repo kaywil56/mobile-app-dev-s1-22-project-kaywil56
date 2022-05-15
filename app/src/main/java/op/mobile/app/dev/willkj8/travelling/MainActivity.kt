83a1741ac580f506329a103e1be1ae7cd2f57df9
Kaylem W <willkj8@student.op.ac.nz>
2022-05-15 13:09:36 +1200

    feat: constant dark mode

--- app/src/main/java/op/mobile/app/dev/willkj8/travelling/MainActivity.kt
+++ app/src/main/java/op/mobile/app/dev/willkj8/travelling/MainActivity.kt
@@ -1,9 +1,10 @@
 package op.mobile.app.dev.willkj8.travelling
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate
+import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
+import androidx.lifecycle.asLiveData
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -13,32 +14,22 @@ import op.mobile.app.dev.willkj8.travelling.helpers.settings.SettingsManager
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val toolbar: Toolbar = findViewById(R.id.toolbar) // Find the View in activity_main.xml with the id toolbar
         setSupportActionBar(toolbar) // Set toolbar as the entire application's action bar
-
-//        settingsManager.uiModeFlow.asLiveData().observe(this, Observer {
-//            when (it) {
-//                UIMode.LIGHT -> {
-//                    // This changes the UI mode to light mode. It will use the default themes.xml
-//                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-//                }
-//                UIMode.DARK -> {
-//                    // This changes the UI mode to dark mode. It will use the night themes.xml
-//                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-//                }
-//            }
-//        })
-
-
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
-        val appBarConfiguration = AppBarConfiguration(
+        navController = navHostFragment.navController
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        //val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        //val navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_home,
                 R.id.navigation_dashboard,
@@ -46,7 +37,13 @@ class MainActivity : AppCompatActivity() {
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
+
+        //setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-        supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        //supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val settingsManager = SettingsManager(this)
+        settingsManager.uiModeFlow.asLiveData().observe(this) {
+            settingsManager.setCheckedUIMode(it, true, null)
+        }
     }
 }
--- app/src/main/java/op/mobile/app/dev/willkj8/travelling/helpers/settings/SettingsManager.kt
+++ app/src/main/java/op/mobile/app/dev/willkj8/travelling/helpers/settings/SettingsManager.kt
@@ -1,6 +1,8 @@
 package op.mobile.app.dev.willkj8.travelling.helpers.settings
 
 import android.content.Context
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
 import androidx.datastore.DataStore
 import androidx.datastore.preferences.*
 import kotlinx.coroutines.flow.Flow
@@ -8,14 +10,13 @@ import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
 import java.io.IOException
 
-class SettingsManager(context: Context) {
+class SettingsManager(val context: Context) {
     private val dataStore: DataStore<Preferences> =
         context.applicationContext.createDataStore(DATA_STORE_NAME)
-
     val uiModeFlow: Flow<UIMode> = dataStore.data.catch {
         if (it is IOException) {
             it.printStackTrace()
-            emit(emptyPreferences())
+            emit((emptyPreferences()))
         } else {
             throw it
         }
@@ -25,7 +26,6 @@ class SettingsManager(context: Context) {
             false -> UIMode.LIGHT
         }
     }
-
     suspend fun setUIMode(uiMode: UIMode) {
         dataStore.edit {
             it[IS_DARK_MODE] = when (uiMode) {
@@ -34,7 +34,54 @@ class SettingsManager(context: Context) {
             }
         }
     }
-
+    private fun uiModeFeatures(
+        mode: Int,
+        isMainActivity: Boolean,
+        swUIMode: SwitchCompat?,
+        //googleMap: GoogleMap?,
+        //mapStyle: Int,
+        isUIModeChecked: Boolean
+    ) {
+        AppCompatDelegate.setDefaultNightMode(mode)
+        /*if (!isMainActivity && swUIMode == null) {
+            googleMap?.setMapStyle(
+                MapStyleOptions.loadRawResourceStyle(
+                    context,
+                    mapStyle
+                )
+            )
+        }*/
+        swUIMode?.isChecked = isUIModeChecked
+    }
+    fun setCheckedUIMode(
+        uiMode: UIMode?,
+        isMainActivity: Boolean,
+        swUIMode: SwitchCompat?,
+        //googleMap: GoogleMap?
+    ) {
+        when (uiMode) {
+            UIMode.LIGHT -> {
+                uiModeFeatures(
+                    AppCompatDelegate.MODE_NIGHT_NO,
+                    isMainActivity,
+                    swUIMode,
+                    //googleMap,
+                    //R.raw.retro_google_map,
+                    false
+                )
+            }
+            UIMode.DARK -> {
+                uiModeFeatures(
+                    AppCompatDelegate.MODE_NIGHT_YES,
+                    isMainActivity,
+                    swUIMode,
+                    //googleMap,
+                    //R.raw.dark_google_map,
+                    true
+                )
+            }
+        }
+    }
     companion object {
         private const val DATA_STORE_NAME = "settings.pref"
         private val IS_DARK_MODE = preferencesKey<Boolean>("is_dark_mode")
