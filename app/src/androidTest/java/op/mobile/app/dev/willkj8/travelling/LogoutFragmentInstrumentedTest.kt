372840f40c327ae6e6c0d990e349ef0dd11ac78c
Kaylem W <willkj8@student.op.ac.nz>
2022-05-27 10:42:24 +1200

    feat: test logout

--- app/src/androidTest/java/op/mobile/app/dev/willkj8/travelling/LogoutFragmentInstrumentedTest.kt
+++ app/src/androidTest/java/op/mobile/app/dev/willkj8/travelling/LogoutFragmentInstrumentedTest.kt
@@ -1,18 +1,21 @@
 package op.mobile.app.dev.willkj8.travelling
 
 import androidx.test.espresso.Espresso
-import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
 import org.hamcrest.core.AllOf
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
-class RegisterFragmentInstrumentedTest {
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class LogoutFragmentInstrumentedTest {
 
     // lateinit allows you to initialise a variable later
     private lateinit var emailAddressToBeTyped: String
@@ -25,29 +28,28 @@ class RegisterFragmentInstrumentedTest {
     @Before
     fun initValidString() {
         // Initialising variables marked with the lateinit keyword
-        emailAddressToBeTyped = "emailfortesting@mail.co.nz"
+        emailAddressToBeTyped = "kaywil56@gmail.com"
         passwordToBeTyped = "12345678"
     }
 
     @Test
-    fun register() {
+    fun logout() {
         // Delay for splash screen
         Thread.sleep(5000)
-        onView(withId(R.id.btn_to_sign_up)).perform(ViewActions.click())
-
-        onView(withId(R.id.et_email_address))
+        // withId(R.id.et_email_address) is a ViewMatcher
+        Espresso.onView(ViewMatchers.withId(R.id.et_email_address))
             // typeText() is a ViewAction
             .perform(ViewActions.typeText(emailAddressToBeTyped), ViewActions.closeSoftKeyboard())
-        onView(withId(R.id.et_password))
+        Espresso.onView(ViewMatchers.withId(R.id.et_password))
             .perform(ViewActions.typeText(passwordToBeTyped), ViewActions.closeSoftKeyboard())
-        onView(withId(R.id.et_confirm_password))
-            .perform(ViewActions.typeText(passwordToBeTyped), ViewActions.closeSoftKeyboard())
-        onView(withId(R.id.btn_register)).perform(ViewActions.click())
+        // click() is a ViewAction
+        Espresso.onView(ViewMatchers.withId(R.id.btn_login)).perform(ViewActions.click())
+        // Give time for login delay
         Thread.sleep(5000)
-        onView(
-            AllOf.allOf(
-                withId(R.id.tv_header), ViewMatchers.withText("Login")
-            )
-        ).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+        Espresso.onView(ViewMatchers.withId(R.id.navigation_settings)).perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.btn_logout)).perform(ViewActions.click())
+
+        Espresso.onView(ViewMatchers.withId(R.id.tv_header)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
     }
 }
